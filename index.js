25a45fbeb87e6d49239c5be3bcd9723f8efe2d05
Tomas Tulka <tomas.tulka@gmail.com>
2021-09-20 20:57:47 +0200

    add comments

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const {Worker} = require("worker_threads");
 const HOST = "localhost";
 const PORT = 8000;
 const CACHE_TTL_MS = 1000;
-const NUMBER_OF_WORKERS = 2;
+const WORKER_POOL_SIZE = 2;
 const EXECUTION_TIMEOUT_MS = 5000;
 
+// general key-value cache with eviction after TTL
 class Cache {
   constructor(ttl = CACHE_TTL_MS) {
     this.ttl = ttl;
@@ -33,6 +34,7 @@ class Cache {
   };
 }
 
+// implements the platform API
 class HttpServer {
   constructor(controllers, host = HOST, port = PORT) {
     this.controllers = controllers;
@@ -63,6 +65,7 @@ class HttpServer {
       res.end(`wrong request ${request.controller}\n`);
     }
   }
+  // parses requests in format `/<controller>/<actio>[?param1[,param2[...]]]`
   _parseRequest({url}) {
     console.log('parsing request', url);
     const [controller, path] = url.substring(1).split("/");
@@ -72,14 +75,13 @@ class HttpServer {
   }
 }
 
+// excutes Wasm module in a worker pool
 class WasmThreadExecutor {
-  constructor(numberOfWorkers = NUMBER_OF_WORKERS) {
-    this.total = numberOfWorkers;
-    this.busy = 0;
-    this.tasks = [];
-    this.modules = new Cache();
-
-    this._work = this._work.bind(this);
+  constructor(poolSize = WORKER_POOL_SIZE) {
+    this.poolSize = poolSize; // max count of workers
+    this.busy = 0;    // currently busy workers
+    this.tasks = [];  // executions are queued as tasks
+    this.modules = new Cache(); // modules are cached to optimize against cold starts
   }
   async execute(wasmFile, params, onFinish) {
     console.debug("executing wasm module", wasmFile, params);
@@ -94,29 +96,34 @@ class WasmThreadExecutor {
         _reject(error);
         onFinish(stats);
       };
-      this.tasks.push({exec, resolve, reject});
+      this.tasks.push({exec, resolve, reject}); // defer execution when there is a free worker
     });
-    this._work();
-    return promise;
+    this._work();   // trigger task polling
+    return promise; // return a promise of a future execution
   }
   async _loadWasmBuffer(wasmFile) {
     console.debug("loading wasm buffer from file", wasmFile);
     return fs.readFileSync(wasmFile);
   }
+  // polls a task from the queue and executes on a free worker
   async _work() {
     console.debug(`attempt to execute in worker, busy workers: ${this.busy}`);
-    if (!this.tasks.length || this.busy >= this.total) {
+    // are there free workers?
+    if (!this.tasks.length || this.busy >= this.poolSize) {
       return;
     }
+    // start execution on a worker
     const time_start = Date.now();
     this.busy++;
     const onFinish = () => {
       this.busy--;
       console.debug(`worker finished, busy workers: ${this.busy}`);
-      this._work();
+      this._work(); // trigger task polling when this worker finished
     };
-    const createStats = () => ({time: Date.now() - time_start});
+    const createStats = () => ({time: Date.now() - time_start});  // collect execution time
+    // pop another task from the queue
     const task = this.tasks.pop();
+    // create a new worker instance
     const worker = new Worker("./worker.js", {
       workerData: task.exec
     });
@@ -127,6 +134,7 @@ class WasmThreadExecutor {
       if (code !== 0) task.reject(`Exit code ${code}`, createStats());
       onFinish();
     });
+    // kill the worker when timeout is reached
     setTimeout(() => {
       console.warn("timout reached; killing worker");
       worker.terminate();
@@ -134,10 +142,11 @@ class WasmThreadExecutor {
   }
 }
 
+// the platform implementation
 class Platform {
   constructor() {
-    this.wasmExecutor = new WasmThreadExecutor();
-    this.registry = new Map();
+    this.wasmExecutor = new WasmThreadExecutor(); // worker-based execution of Wasm modules
+    this.registry = new Map();  // keep registered modules in memory
     this.server = new HttpServer({
       "exec": this.exec.bind(this),
       "register": this.register.bind(this),
@@ -145,20 +154,21 @@ class Platform {
     });
   }
   start() {
-    this.server.start();
+    this.server.start();  // start the HTTP server
   }
   exec(moduleName, params) {
-    if (!this.registry.has(moduleName))  {
+    if (!this.registry.has(moduleName))  {  // execute only already registered modules
       throw new Error(`cannot find module '${moduleName}'`);
     }
     const module = this.registry.get(moduleName);
-    const result = this.wasmExecutor.execute(module.wasmFile, params, ({time}) => module.stats.time += time);
-    return result;
+    // return a promise of a future execution
+    return this.wasmExecutor.execute(
+      module.wasmFile, params, ({time}) => module.stats.time += time);
   }
   register(moduleName, attributes) {
     this.registry.set(moduleName, {
       name: moduleName,
-      wasmFile: `./${moduleName}.wasm`,
+      wasmFile: `./${moduleName}.wasm`, // search for modules by name in the root directory
       stats: {
         time: 0
       }
@@ -170,7 +180,7 @@ class Platform {
       throw new Error(`cannot find module '${moduleName}'`);
     }
     const module = this.registry.get(moduleName);
-    return `execution time: ${module.stats.time}ms`;
+    return `execution time: ${module.stats.time}ms`;  // print stats
   }
 }
 
